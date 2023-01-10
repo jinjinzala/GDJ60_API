51deda960c25f7df17dc6259c783494bcd425dba
jinjinzala <jinjinzala@naver.com>
2023-01-10 15:03:33 +0900

    .

---
+++ ex1/Ex1Main.java
@@ -0,0 +1,44 @@
+package ex1;
+
+import java.util.ArrayList;
+
+public class Ex1Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // 학생은 이름 번호 국어 영어 수학 총점 평균으로 이루어짐
+        // 1. 학생 정보 초기화 
+		
+		// 2. 학생 정보 전체 조회
+		// 3. 학생 정보 검색 조회  이름을 검색하여 사용 
+		// 4. 학생 정보 추가 
+		// 5. 학생 정보 삭제   이름으로 검색하여 사용
+		// 6. 프로그램 종료
+		
+		new StudentController().start();
+		
+		StudentDAO dao = new StudentDAO();
+		ArrayList<StudentDTO> ar = dao.init();
+		StudentDTO studentDTO = dao.findByName(ar);
+		System.out.println(studentDTO.getName());
+		System.out.println(studentDTO.getAvg());
+		
+	    for(int i=0; i<ar.size();i++) {
+			System.out.println(ar.get(i).getName());
+      
+	   int[] nums = new int[3];		
+	   for(int n:nums) {
+		  
+	   }
+			
+		}
+		
+	
+		
+		
+		
+		
+		
+	}
+
+}
